e9f51991f9cbcb5c9055e42b948d5d68dba73123
dikaarnnd <dika4kiga@gmail.com>
2024-10-19 23:28:09 +0700

    update

--- Project-UTS/home.php
+++ Project-UTS/home.php
@@ -19,7 +19,7 @@
             <h1 class="text-5xl">Hi There!</h1>
             <h1 class="text-7xl">I'm <span class="rubik">DIKA</span></h1>
             <div class="py-2">
-                <h3 class="bg-orange-400 px-2 py-1">Web Programmer, Network Engineer</h3>
+                <h3 class="bg-orange-400 px-2 py-1 w-max">Web Programmer, Network Engineer</h3>
             </div>
             <p class="text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div class="my-3">
