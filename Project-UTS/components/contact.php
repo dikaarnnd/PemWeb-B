66b95c6ba19736463fd768930b4dc6885d904a77
dikaarnnd <dika4kiga@gmail.com>
2024-10-24 22:30:23 +0700

    update

---
+++ Project-UTS/components/contact.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../style/font.css">
+    <!-- Bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- tailwind cdn -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Contact</title>
+</head>
+<body>
+    <?php
+        $server = "localhost";
+        $user   = "root";
+        $pw     = "";
+        $namaDB = "uts";
+        $port   = 3306;
+        $conn = mysqli_connect($server, $user, $pw, $namaDB, $port);
+
+        // Cek koneksi
+        if ($conn->connect_error) {
+            die("Koneksi gagal: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT link FROM content WHERE namafoto='orang'";
+        $result = $conn->query($sql);
+
+        $link = '';
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $link = $row["link"];
+            }
+        } else {
+            $link = "default.jpg";
+        }
+
+        $conn->close();
+    ?>
+    <div class="flex min-[320px]:flex-col max-[768px]:flex-col min-[769px]:flex-row h-screen signika">
+        <div class="flex flex-col basis-1/5">
+            <div class="">
+                <img src="<?php echo $link;?>" class="w-full"/>
+            </div>
+            <nav class="bg-yellow-400 text-white h-full flex flex-col justify-center items-center">
+                <div class="w-1 h-1 rounded-full bg-black my-8"></div>
+                <a href="./home.php" class="mb-3 text-black font-bold hover:text-white">HOME</a>
+                <a href="./about.php" class="mb-3 text-black font-bold hover:text-white">ABOUT ME</a>
+                <a href="./skills.php" class="mb-3 text-black font-bold hover:text-white">SKILL</a>
+                <a href="./project.php" class="mb-3 text-black font-bold hover:text-white">MY PROJECT</a>
+                <a href="./contact.php" class="mb-3 text-white font-bold hover:text-white">CONTACT</a>
+                <div class="w-1 h-1 rounded-full bg-black my-8"></div>
+            </nav>
+        </div>
+        <div class="flex flex-col basis-4/5 bg-slate-200 p-7">
+            <div class="border-1 border-slate-500 py-2 text-6xl w-full mb-4 text-center">
+                <h1 class="rubikmono">CONTACT ME</h1>
+            </div>
+            <div class="mt-2 mb-3">
+                <h2 class="text-2xl text-center font-bold">What I Do?</h2>
+            </div>
+            <div class="flex justify-evenly">
+                <div class="basis-1/3 place-items-center bg-yellow-300 m-2">
+                    <img src="./images/iwak_200.jpeg" class="w-1/3"/>
+                    <h5 class="text-center font-bold pt-1">Web Design</h5>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="basis-1/3 place-items-center bg-yellow-300 m-2">
+                    <img src="./images/iwak_200.jpeg" class="w-1/3"/>
+                    <h5 class="text-center font-bold pt-1">Web Design</h5>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="basis-1/3 place-items-center bg-yellow-300 m-2">
+                    <img src="./images/iwak_200.jpeg" class="w-1/3"/>
+                    <h5 class="text-center font-bold pt-1">Web Design</h5>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ Project-UTS/components/home.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../style/font.css">
+    <!-- Bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- tailwind cdn -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Home</title>
+</head>
+<body>
+    <?php
+        $server = "localhost";
+        $user   = "root";
+        $pw     = "";
+        $namaDB = "uts";
+        $port   = 3306;
+        $conn = mysqli_connect($server, $user, $pw, $namaDB, $port);
+
+        // Cek koneksi
+        if ($conn->connect_error) {
+            die("Koneksi gagal: " . $conn->connect_error);
+        }
+
+        $sql1 = "SELECT link FROM content WHERE namafoto='arduino'";
+        $result1 = $conn->query($sql1);
+        $sql2 = "SELECT namadepan FROM biodata WHERE id=1";
+        $result2 = $conn->query($sql2);
+        $sql3 = "SELECT skillname FROM skill";
+        $result3 = $conn->query($sql3);
+
+        $link = '';
+        if ($result1->num_rows > 0) {
+            while($row = $result1->fetch_assoc()) {
+                $link = $row["link"];
+            }
+        } else {
+            $link = "default.jpg";
+        }
+
+        $namaDepan = '';
+        if ($result2->num_rows > 0) {
+            while($row = $result2->fetch_assoc()) {
+                $namaDepan = $row["namadepan"];
+            }
+        } else {
+            $namaDepan = "default.jpg";
+        }
+
+        $namaSkill = [];
+        if ($result3->num_rows > 0) {
+            while($row = $result3->fetch_assoc()) {
+                $namaSkill [] = $row["skillname"];
+            }
+        } else {
+            $namaSkill = "default.jpg";
+        }
+        $allSkill = !empty($namaSkill) ? implode(', ', $namaSkill) : "default skill"; 
+
+        $conn->close();
+    ?>
+    <div class="flex min-[320px]:flex-col max-[768px]:flex-col min-[769px]:flex-row h-screen signika">
+        <div class="basis-2/5">
+            <img src="<?php echo $link; ?>" class="h-full w-full"/>
+        </div>
+        <div class="basis-3/5 bg-slate-200 px-5 py-6 content-center">
+            <h1 class="text-5xl">Hi There!</h1>
+            <h1 class="text-7xl">I'm <span class="rubik"><?php echo $namaDepan;?></span></h1>
+            <div class="py-2">
+                <h3 class="bg-orange-400 px-2 py-1 w-max font-bold"><?php echo $allSkill;?></h3>
+            </div>
+            <div class="my-3">
+                <a href="./about.php" class="bg-orange-300 p-2 rounded-lg hover:bg-orange-500 hover:text-black transition duration-400 ease-in-out">More about me</a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
