5126c3fd4369a13e45be58d35ec5085757e078ab
dikaarnnd <dika4kiga@gmail.com>
2024-11-15 15:55:03 +0700

    w

--- pemrograman-web-b/routes/web.php
+++ pemrograman-web-b/routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/helo', function () {
+    return 'Apa kabar dunia';
+});
+
+Route::get('/belajar', function () {
+    echo '<h1>Apa kabar dunia</h1>';
+    echo 'tetep asikkkk....';
+});
+
+Route::get('/biodata/{namadepan}/{namabelakang}', function ($namadepan, $namabelakang) {
+    return "<h1>Apa kabar dunia laravel</h1>
+    Selamat datang ". $namadepan." ". $namabelakang;
+});
